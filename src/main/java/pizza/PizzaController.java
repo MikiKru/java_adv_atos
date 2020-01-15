45b0f53818c2f06dd2093a224b3c1a0522c4dbed
MikiKru <michal_kruczkowski@o2.pl>
2020-01-15 12:54:34 +0100

    project start

---
+++ src/main/java/pizza/PizzaController.java
@@ -0,0 +1,15 @@
+package pizza;
+
+public class PizzaController {
+// Pizza findCheapestSpicy() - metoda zwracająca najtańszą ostrą pizzę.
+
+// Pizza findMostExpensiveVegetarian() - metoda zwracająca najdroższą pizzę wegetariańską.
+
+// List iLikeMeat() - metoda zwracająca same pizzę mięsne, posortowane malejąco po liczbie składników mięsnych.
+
+// Map groupByPrice() - metoda grupujące pizzę po cenie.
+
+// String formatedMenu() - metoda zwracająca string w postaci nazwa_pizzy: składnik1, składnik2, składnik3 - cena, kolejne pizzę oddzielone znakiem nowej linii.
+
+
+}
