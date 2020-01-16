a2c4fc0307340b72f6fb9448f5225837ec339b4e
MikiKru <michal_kruczkowski@o2.pl>
2020-01-16 08:50:11 +0100

    project start

---
+++ src/main/java/oop/User.java
@@ -0,0 +1,40 @@
+package oop;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data       // getter setter toString
+public class User {
+    private static Long globalId = 0L;
+    private long userId;
+    private String login;
+    private String password;
+    private LocalDateTime registrationDate;
+    private boolean status;
+
+    private static Long auto_increment(){
+        return ++globalId;
+    }
+
+    public User(String login, String password) {
+        System.out.println("Konstruktor USER");
+        this.userId = auto_increment();
+        this.login = login;
+        this.password = password;
+        this.registrationDate = LocalDateTime.now();
+        this.status = false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(User.globalId);
+        User user1 = new User("m", "m");
+        User user2 = new User("k", "k");
+        User user3 = new User("t", "t");
+        System.out.println(user1);
+        System.out.println(user2);
+        System.out.println(user3);
+    }
+}
