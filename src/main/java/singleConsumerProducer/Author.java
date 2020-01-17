7317db203d833682e6a3033dcc8e100f9b1fc64f
MikiKru <michal_kruczkowski@o2.pl>
2020-01-17 12:14:56 +0100

    thread coordination with timeout

--- src/main/java/singleConsumerProducer/Author.java
+++ src/main/java/singleConsumerProducer/Author.java
@@ -12,8 +12,29 @@ public class Author extends Thread {
     public void run(){
         for (int i = 0;  i < textToPublish.length; i++){
             try {
-                sleep(new Random().nextInt(5000));
-                text.setTextToWrite(textToPublish[i]);
+                Thread timeTh = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            sleep(2000);
+
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }) ;
+                int sleepTime = new Random().nextInt(5000);
+                System.out.println("Czas uśpienia: " + sleepTime);
+                if(sleepTime < 2000){
+                    sleep(sleepTime);
+                    text.setTextToWrite(textToPublish[i]);
+                } else if(timeTh.isAlive()) {
+                    timeTh.start();
+                    timeTh.join();
+                    text.setTextToWrite(textToPublish[i]);
+                } else {
+                    System.out.println("Nic nie opublikowano");
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
